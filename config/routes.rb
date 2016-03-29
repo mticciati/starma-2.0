a2415ea2102759d60333e5460c4be4c73fb1ada6
mticciati <matthewticciati@gmail.com>
2016-03-29 14:13:23 -0700

    setting up

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,6 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
   root 'pages#welcome'
 
   # static pages - need new way to avoid having to update all files when adding a static page
