248f5828d44ff2183409be17fd05e15a27c25305
mticciati <matthewticciati@gmail.com>
2016-03-31 16:34:37 -0700

    send messages now works

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -18,10 +18,9 @@ class MessagesController < ApplicationController
 
 	def create
 		@message = Message.new(message_params)
-		@message.from_user_id = current_user
 		if @message.save
 			flash[:sucess] = "Message Sent!"
-			redirect_to root
+			redirect_to root_path
 			# redirect_to whatever user was last viewing
 		else
 			flash[:danger] = "We can't send an empty message!"
@@ -32,7 +31,7 @@ class MessagesController < ApplicationController
 	private
 
 	def message_params
-		params.require(:message).permit(:body, :to_user_id)
+		params.require(:message).permit(:body, :to_user_id).merge(from_user_id: current_user.id)
 	end
 
 end
