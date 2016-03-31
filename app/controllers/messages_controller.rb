32dc61f34e1c6703b5acc9a811e00ec2486bc98b
mticciati <matthewticciati@gmail.com>
2016-03-31 15:45:18 -0700

    flash types to application and devise layouts, validates body for Message, redirect to new message when errors with query string with user id

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -13,10 +13,26 @@ class MessagesController < ApplicationController
 	def new
 		@to = User.find(params['user'])
 		@message = Message.new
+		@message.to_user_id = @to.id
 	end
 
 	def create
-		@message
+		@message = Message.new(message_params)
+		@message.from_user_id = current_user
+		if @message.save
+			flash[:sucess] = "Message Sent!"
+			redirect_to root
+			# redirect_to whatever user was last viewing
+		else
+			flash[:danger] = "We can't send an empty message!"
+			redirect_to new_message_path(user: @message.to_user_id)
+		end
+	end
+
+	private
+
+	def message_params
+		params.require(:message).permit(:body, :to_user_id)
 	end
 
 end
