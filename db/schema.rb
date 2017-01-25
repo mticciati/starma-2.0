c67e44d0b37f6a1e6ad77c5342c2cf0e6d226e7d
mticciati <matthewticciati@gmail.com>
2017-01-25 11:00:39 -0800

    adding roles to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170114191252) do
+ActiveRecord::Schema.define(version: 20170125185843) do
 
   create_table "conversations", force: :cascade do |t|
     t.integer  "sender_id",    limit: 4, null: false
@@ -63,20 +63,20 @@ ActiveRecord::Schema.define(version: 20170114191252) do
   add_index "poi_positions", ["western_chart_type", "western_chart_id"], name: "index_poi_positions_on_western_chart_type_and_western_chart_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  limit: 255, default: "", null: false
-    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "email",                  limit: 255, default: "",     null: false
+    t.string   "encrypted_password",     limit: 255, default: "",     null: false
     t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
+    t.integer  "sign_in_count",          limit: 4,   default: 0,      null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip",     limit: 255
     t.string   "last_sign_in_ip",        limit: 255
-    t.datetime "created_at",                                      null: false
-    t.datetime "updated_at",                                      null: false
+    t.datetime "created_at",                                          null: false
+    t.datetime "updated_at",                                          null: false
     t.date     "birthday"
-    t.string   "username",               limit: 255,              null: false
+    t.string   "username",               limit: 255,                  null: false
     t.string   "location",               limit: 255
     t.float    "latitude",               limit: 24
     t.float    "longitude",              limit: 24
@@ -87,6 +87,7 @@ ActiveRecord::Schema.define(version: 20170114191252) do
     t.boolean  "time_unknown"
     t.integer  "time_interval",          limit: 4
     t.boolean  "dst"
+    t.string   "role",                   limit: 255, default: "user"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
